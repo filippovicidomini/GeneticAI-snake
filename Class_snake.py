3f976788f3bfd6fc163baf1df5d49d31336f7034
Filippo Vicidomini <filippovicidomini@gmail.com>
2023-04-14 20:15:03 +0200

    trova giuste le distanze :)

--- Class_snake.py
+++ Class_snake.py
@@ -1,8 +1,8 @@
 # now i want code snake but withouth the pygame and with a neural network that will control the snake and try to get the highest score possible
 
+from random import randint
+
 import numpy as np
-import random
-import math
 
 
 class Snake:
@@ -15,13 +15,13 @@ class Snake:
 
     def put_snake(self, random=True):
         if random:
-            self.grid[random.randomint(0, len(self.grid)), random.randomint(0, len(self.grid))] = 1
+            self.grid[randint(0, len(self.grid) - 1), randint(0, len(self.grid) - 1)] = 1
         else:
             self.grid[0, 0] = 1     # parte dal angolo in alto a sinistra
 
     def put_food(self, random=True):
         if random:
-            self.grid[random.randomint(0, len(self.grid)), random.randomint(0, len(self.grid))] = 2
+            self.grid[randint(0, len(self.grid) - 1), randint(0, len(self.grid) - 1)] = 2
         else:
             self.grid[-1, -1] = 2   # parte dal angolo in basso a destra
 
@@ -29,26 +29,20 @@ class Snake:
         self.position_of_snake = np.where(self.grid == 1)
         self.position_of_food = np.where(self.grid == 2)
         # devo trovare la distanza di manhattan tra il serpente e il cibo
-        self.distance_from food = abs(self.position_of_snake[0] - self.position_of_food[0]) + abs(self.position_of_snake[1] - self.position_of_food[1])
-        return self.distance_from_food
+        self.distance_from_food = abs(self.position_of_snake[0] - self.position_of_food[0]) + abs(
+            self.position_of_snake[1] - self.position_of_food[1])
+        return self.distance_from_food[0]
 
-    def get_distance_from_wall(self):   # distanza tra le 4 mura
+    def get_distance_from_wall(self):  # distanza tra le 4 mura
+        # sinistra, destra, sotto, sopra
         self.position_of_snake = np.where(self.grid == 1)
-        self.distance_wall = [self.position_of_snake[0], len(self.grid) - self.position_of_snake[0], self.position_of_snake[1], len(self.grid) - self.position_of_snake[1]]
-        return self.distance_wall
-
-    def game_over(self):
-        self.position_of_snake = np.where(self.grid == 1)
-        if self.position_of_snake[0] == 0 or self.position_of_snake[0] == len(self.grid) - 1 or self.position_of_snake[1] == 0 or self.position_of_snake[1] == len(self.grid) - 1:
-            return True
-        else:
-            return False
-
+        self.distance_wall = [int(self.position_of_snake[0]), int(len(self.grid) - self.position_of_snake[0]),
+                              int(self.position_of_snake[1]), int(len(self.grid) - self.position_of_snake[1])]
+        return list(self.distance_wall[:])
 
     def move(self, direction):
         if direction == 0:
             self.grid[self.position_of_snake[0], self.position_of_snake[1] + 1] = 1
-
         elif direction == 1:
             self.grid[self.position_of_snake[0], self.position_of_snake[1] - 1] = 1
         elif direction == 2:
@@ -56,10 +50,26 @@ class Snake:
         elif direction == 3:
             self.grid[self.position_of_snake[0] - 1, self.position_of_snake[1]] = 1
 
+    def output(self):
+        # i want to display the grid S if is the snake, F if is the food and . if is nothing
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid)):
+                if self.grid[i, j] == 0:
+                    print("_", end="  ")
+                elif self.grid[i, j] == 1:
+                    print("S", end="  ")
+                elif self.grid[i, j] == 2:
+                    print("F", end="  ")
+            print()
 
     def play(self):
         self.workspace(10)
         self.put_snake()
         self.put_food()
-        while True:
-            self.move()
+
+
+if __name__ == "__main__":
+    game = Snake()
+    game.play()
+    game.output()
+    print(game.get_distance_from_food())
