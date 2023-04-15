e34ca336aa875bbb56f7ab2f978f891971f34e15
Filippo Vicidomini <filippovicidomini@gmail.com>
2023-04-15 13:41:40 +0200

    fa un passo alla volta eliminando la posizione precente

--- Class_snake.py
+++ Class_snake.py
@@ -7,7 +7,9 @@ import numpy as np
 
 class Snake:
     def __init__(self):
-        self.score = 1 # one because the snake start with one point
+        self.score = 1  # one because the snake start with one point
+        self.snake_head = None  # is a tuple with the coordinates of the snake head
+        snake_body = None  # is a list with the coordinates of the snake
         self.brain = np.random.normal(0, 1, (5, 4))
 
     def workspace(self, num):
@@ -17,7 +19,8 @@ class Snake:
         if random:
             self.grid[randint(0, len(self.grid) - 1), randint(0, len(self.grid) - 1)] = 1
         else:
-            self.grid[0, 0] = 1     # parte dal angolo in alto a sinistra
+            self.grid[0, 0] = 1  # parte dal angolo in alto a sinistra
+        self.snake_head = np.where(self.grid == 1)
 
     def put_food(self, random=True):
         if random:
@@ -40,16 +43,6 @@ class Snake:
                               int(self.position_of_snake[1]), int(len(self.grid) - self.position_of_snake[1])]
         return list(self.distance_wall[:])
 
-    def move(self, direction):
-        if direction == 0:
-            self.grid[self.position_of_snake[0], self.position_of_snake[1] + 1] = 1
-        elif direction == 1:
-            self.grid[self.position_of_snake[0], self.position_of_snake[1] - 1] = 1
-        elif direction == 2:
-            self.grid[self.position_of_snake[0] + 1, self.position_of_snake[1]] = 1
-        elif direction == 3:
-            self.grid[self.position_of_snake[0] - 1, self.position_of_snake[1]] = 1
-
     def output(self):
         # i want to display the grid S if is the snake, F if is the food and . if is nothing
         for i in range(len(self.grid)):
@@ -61,38 +54,46 @@ class Snake:
                 elif self.grid[i, j] == 2:
                     print("F", end="  ")
             print()
+        print('\n')
 
     def play(self):
         self.workspace(10)
         self.put_snake()
         self.put_food()
 
+    def get_score(self):
+        return self.score
+
+    def choose_direction(self):
+        direction = np.random.randint(0, 4)
+        return direction
+
     def move_snake(self, direction):
         if direction == 0:
-            self.grid[self.position_of_snake[0], self.position_of_snake[1] + 1] = 1
-            if self.grid[self.position_of_snake[0], self.position_of_snake[1] + 1] == 2:
-                self.score += 1
-                self.put_food()
+            self.grid[self.snake_head[0], self.snake_head[1] + 1] = 1
+            self.grid[self.snake_head[0], self.snake_head[1]] = 0
+            self.snake_head = np.where(self.grid == 1)
         elif direction == 1:
-            self.grid[self.position_of_snake[0], self.position_of_snake[1] - 1] = 1
-            if self.grid[self.position_of_snake[0], self.position_of_snake[1] - 1] == 2:
-                self.score += 1
-                self.put_food()
+            self.grid[self.snake_head[0], self.snake_head[1] - 1] = 1
+            self.grid[self.snake_head[0], self.snake_head[1]] = 0
+            self.snake_head = np.where(self.grid == 1)
         elif direction == 2:
-            self.grid[self.position_of_snake[0] + 1, self.position_of_snake[1]] = 1
-            if self.grid[self.position_of_snake[0] + 1, self.position_of_snake[1]] == 2:
-                self.score += 1
-                self.put_food()
+            self.grid[self.snake_head[0] - 1, self.snake_head[1]] = 1
+            self.grid[self.snake_head[0], self.snake_head[1]] = 0
+            self.snake_head = np.where(self.grid == 1)
         elif direction == 3:
-            self.grid[self.position_of_snake[0] - 1, self.position_of_snake[1]] = 1
-            if self.grid[self.position_of_snake[0] - 1, self.position_of_snake[1]] == 2:
-                self.score += 1
-                self.put_food()
+            self.grid[self.snake_head[0] + 1, self.snake_head[1]] = 1
+            self.grid[self.snake_head[0], self.snake_head[1]] = 0
+            self.snake_head = np.where(self.grid == 1)
+        self.score += 1
+
+
 
 if __name__ == "__main__":
     game = Snake()
     game.play()
     game.output()
     print(game.get_distance_from_food())
-    game.move_snake(0)
-    game.output()
+    for i in range(10):
+        game.move_snake(game.choose_direction())
+        game.output()
